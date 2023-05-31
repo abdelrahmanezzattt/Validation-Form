908aeb7a3aaca037d401b3d045eb472466e2815d
abdelrahmanezzattt <119177660+abdelrahmanezzattt@users.noreply.github.com>
2023-05-31 23:34:11 +0300

    Add files via upload

---
+++ sec.js
@@ -0,0 +1,35 @@
+// var x="ahmed"
+//  for(let y in x)
+//  {
+//     document.write(y);
+//     document.write('<br>');
+//  }
+
+//  for( let x=0; x<10;x++)
+//  {
+//     document.write("hello");
+//  }
+//  for(let i=10;i>=10;i--)
+//  {
+//     if (i%2==0){
+//         document.write("<h1 style='color:red'>" "+ i + " is even number</h1>");
+//     }
+//     else{
+//         document.write("i'+ is odd number'")
+//     }
+//  }
+
+// var tot=0;
+// for(let x =1 ;x <= 10;x++){
+//     var num=parseInt(prompt("enter any num ","num here"));
+//     if(num%2==0){
+//         tot=tot+num;
+//     }
+//     document.write("the sum=", tot);
+    
+// }
+// function tot (x,y){
+//     var z= x+y;
+//     return z;
+// }
+// document.write(tot(5,3))
